a27b700240f56efa09f0b45c1f84928b27f84a80
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-05 22:00:26 +0100

    feat: task 3 solution

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+
+Default flask route
+
+"""
+from flask import Flask, render_template
+from flask_babel import Babel, request, _
+
+
+app = Flask(__name__)
+
+
+class Config():
+    '''config class'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''determine match'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    '''default index route'''
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
