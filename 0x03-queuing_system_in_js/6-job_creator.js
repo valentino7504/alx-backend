63d98ff803fdb02da3d6e542a749d34e6639f397
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-04 20:27:24 +0100

    feat: task 6 solution

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,22 +1,19 @@
-#!/usr/bin/node
-/**
- * Create a job
- */
-import { createQueue } from 'kue';
+import { createQueue } from "kue";
 
 const queue = createQueue();
-const jobData = { phoneNumber: '+2347065345423', message: 'Kindly verify your identification' };
 
-const job = queue
-  .create('push_notification_code', jobData)
-  .save((err) => {
-    if (!err) console.log(`Notification job created: ${job.id}`);
-  });
+const job = queue.create('push_notification_code', {
+  phoneNumber: '08056789654',
+  message: 'Account has been created'
+})
 
-job.on('complete', (result) => { /* eslint-disable-line no-unused-vars */
-  console.log('Notification job completed');
-});
-
-job.on('failed', (err) => { /* eslint-disable-line no-unused-vars */
-  console.log('Notification job failed');
-});
+job
+  .on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  })
