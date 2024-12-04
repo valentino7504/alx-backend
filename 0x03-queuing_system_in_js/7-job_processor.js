5f570f9ff8e2211f33fc1d4a3cdb8b65e9b4b477
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-04 23:11:01 +0100

    feat: task 7 solution

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,33 +1,20 @@
-#!/usr/bin/node
-/**
- * Track progress and errors with Kue: Create the Job processor
- */
 import { createQueue } from 'kue';
 
-const blacklist = ['4153518780', '4153518781'];
+const blackList = ['4153518780', '4153518781']
 
-const queue = createQueue();
-
-function sendNotification(phoneNumber, message, job, done) {
-  const total = 100;
-  function next(p) {
-    if (p === 0 || p === (total / 2)) {
-      job.progress(p, total);
-      if (p === (total / 2)) {
-        console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-      }
-    }
-    if (blacklist.includes(job.data.phoneNumber)) {
-      return done(new Error(`Phone number ${job.data.phoneNumber} is blacklisted`));
-    }
-    if (p === total) {
-      return done();
-    }
-    return next(p + 1);
+const sendNotification = (phoneNumber, msg, job, done) => {
+  job.progress(0, 100);
+  if (blackList.includes(phoneNumber)) {
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
   }
-  next(0);
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message ${msg}`);
+  done();
 }
 
+const queue = createQueue();
 queue.process('push_notification_code_2', 2, (job, done) => {
-  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
 });
